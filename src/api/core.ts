9bbafd8f52e18cab83ca08ed9a0aa209dd61aa03
iskan-iskra <bf2iskan@gmail.com>
2024-06-30 01:06:15 +0500

    fix: fix core api url

--- src/api/core.ts
+++ src/api/core.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://test-json-server-ten.vercel.app/api/";
+const baseURL = "https://test-todo-server.vercel.app/api/";
 
 const httpClient = axios.create({
   baseURL,
